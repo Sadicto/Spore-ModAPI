3227345ff8a0bef8df565fad8f50337667f1dd41
Sad <Saidicto@gmail.com>
2026-07-12 03:05:59 -0300

    Add more context for the flag 1 << 3 in cStarRecord

--- Spore ModAPI/Spore/Simulator/cStarRecord.h
+++ Spore ModAPI/Spore/Simulator/cStarRecord.h
@@ -38,7 +38,7 @@ namespace Simulator
 		kStarFlagPotentialSaveGame = 0x1,  // 1 << 0
 		kStarFlagSaveGame = 0x2,  // 1 << 1
 
-		// 1 << 3 has monolith? Also set on some stars without a monolith.
+		// 1 << 3 has monolith? Also set on some stars without a monolith and on the home star during part of the tutorial.
 
 		kStarFlagIsOwned = 0x10,  // 1 << 4
 		
